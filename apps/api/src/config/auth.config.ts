350782c8eb57b9e4505ea7e590ffb9489c15af7a
Fernando <fernando@juandemariana.org>
2025-12-22 17:45:34 +0100

    Add src/config directory.

---
+++ apps/api/src/config/auth.config.ts
@@ -0,0 +1,6 @@
+export const authConfig = {
+  jwtSecret: '***REMOVED***',
+  jwtExpiresIn: 900, // 15 minutos en segundos
+  refreshSecret: '***REMOVED***',
+  refreshExpiresIn: 604800, // 7 días en segundos
+};
