5ecdb6faff12235c720e13a0618b775a9d487f7c
Fernando <fernando@juandemariana.org>
2025-12-23 18:50:17 +0100

    Add default Footer Logo web.

---
+++ apps/web/components/FooterLogo.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <img src="/logo-white.png" alt="Logo" class="h-10 w-auto mb-4" />
+    <p class="text-sm">The ultimate monorepo starter for NestJS + Nuxt 3 + Expo</p>
+  </div>
+</template>
