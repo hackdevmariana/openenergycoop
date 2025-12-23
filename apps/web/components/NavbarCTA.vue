7f21360d33bfed446b21e4325c241abe0c69f009
Fernando <fernando@juandemariana.org>
2025-12-23 18:37:13 +0100

    Add default CTA Navbar web.

---
+++ apps/web/components/NavbarCTA.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex items-center space-x-4">
+    <NuxtLink to="/login" class="btn btn-outline">Login</NuxtLink>
+    <NuxtLink to="/register" class="btn btn-primary">Register</NuxtLink>
+  </div>
+</template>
