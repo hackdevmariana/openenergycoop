1255fa4ec7b5d7ae5a643fff24ffeb90909b7708
Fernando <fernando@juandemariana.org>
2025-12-23 18:37:37 +0100

    Add default DayNightSwitch Navbar web.

---
+++ apps/web/components/NavbarDayNightSwitch.vue
@@ -0,0 +1,6 @@
+<template>
+  <button @click="$colorMode.preference = $colorMode.preference === 'dark' ? 'light' : 'dark'" class="p-2 rounded">
+    <Icon name="heroicons:moon" v-if="$colorMode.preference === 'light'" class="h-6 w-6" />
+    <Icon name="heroicons:sun" v-else class="h-6 w-6 text-yellow-400" />
+  </button>
+</template>
