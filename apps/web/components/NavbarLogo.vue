8a7fc1325c7db2cc134d33ed17f56e55fa8d8c47
Fernando <fernando@juandemariana.org>
2025-12-23 18:36:55 +0100

    Add default Navbar logo web.

---
+++ apps/web/components/NavbarLogo.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex items-center">
+    <img src="/logo.png" alt="Logo" class="h-10 w-auto" />
+    <span class="ml-3 text-2xl font-bold text-primary">Boilerplate</span>
+  </div>
+</template>
