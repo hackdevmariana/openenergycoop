1e1773bea0d600f44a3e93c3ed1aad14e8881870
Fernando <fernando@juandemariana.org>
2025-12-23 18:37:55 +0100

    Add default MenuItems Navbar web.

---
+++ apps/web/components/NavbarMenuItems.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex items-center space-x-8">
+    <NuxtLink to="/" class="text-gray-700 dark:text-gray-300 hover:text-primary">Home</NuxtLink>
+    <NuxtLink to="/dashboard" class="text-gray-700 dark:text-gray-300 hover:text-primary">Dashboard</NuxtLink>
+    <NuxtLink to="/about" class="text-gray-700 dark:text-gray-300 hover:text-primary">About</NuxtLink>
+  </div>
+</template>
