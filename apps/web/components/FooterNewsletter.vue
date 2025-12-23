ae9e1abd907792f840b8c9a3881f8e06b1fd5e1d
Fernando <fernando@juandemariana.org>
2025-12-23 18:50:44 +0100

    Add default Footer Newsletter web.

---
+++ apps/web/components/FooterNewsletter.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h3 class="font-bold mb-4">Newsletter</h3>
+    <form class="flex flex-col sm:flex-row gap-2">
+      <input type="email" placeholder="Your email" class="px-4 py-2 rounded text-gray-900" />
+      <button class="btn btn-primary">Subscribe</button>
+    </form>
+  </div>
+</template>
