27be20d68c288cb911654859dbd745edf301c43f
Fernando <fernando@juandemariana.org>
2026-01-02 14:58:19 +0100

    Fix Navbar day/night switch component to web project.

--- apps/web/components/NavbarDayNightSwitch.vue
+++ apps/web/components/NavbarDayNightSwitch.vue
@@ -1,6 +1,5 @@
 <template>
-  <button @click="$colorMode.preference = $colorMode.preference === 'dark' ? 'light' : 'dark'" class="p-2 rounded">
-    <Icon name="heroicons:moon" v-if="$colorMode.preference === 'light'" class="h-6 w-6" />
-    <Icon name="heroicons:sun" v-else class="h-6 w-6 text-yellow-400" />
-  </button>
+  <UButton color="gray" variant="ghost" @click="$colorMode.preference = $colorMode.preference === 'dark' ? 'light' : 'dark'">
+    <UIcon :name="$colorMode.preference === 'dark' ? 'i-heroicons-sun' : 'i-heroicons-moon'" class="h-5 w-5" />
+  </UButton>
 </template>
