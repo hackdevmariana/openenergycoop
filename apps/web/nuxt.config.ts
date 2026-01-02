ac2db61a99bdd0ef932359ee41793c7abd7401fa
Fernando <fernando@juandemariana.org>
2026-01-02 14:52:42 +0100

    Fix nuxt.config.ts to web project.

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,5 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/ui',
+    '@nuxtjs/color-mode',
+    'nuxt-icon',
+  ],
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'light', // fallback value if not system preference found
+    classSuffix: ''
+  }
 })
