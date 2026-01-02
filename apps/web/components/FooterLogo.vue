9541bcfbee112b25adab0913aa152bcd4fdc9ef0
Fernando <fernando@juandemariana.org>
2026-01-02 14:53:47 +0100

    Fix FooterLogo component to web project.

--- apps/web/components/FooterLogo.vue
+++ apps/web/components/FooterLogo.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <img src="/logo-white.png" alt="Logo" class="h-10 w-auto mb-4" />
-    <p class="text-sm">The ultimate monorepo starter for NestJS + Nuxt 3 + Expo</p>
+    <UIcon name="i-heroicons-cube" class="h-10 w-10 text-primary" />
+    <p class="mt-2 text-sm">NestJS + Nuxt 3 + Expo</p>
   </div>
 </template>
