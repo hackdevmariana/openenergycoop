5e8f08fe458ef06e6c0afe6d18efd4b14b4ea5f2
Fernando <fernando@juandemariana.org>
2026-01-02 14:57:26 +0100

    Fix Content component to web project.

--- apps/web/components/Content.vue
+++ apps/web/components/Content.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="py-16 bg-gray-50 dark:bg-gray-800">
-    <div class="max-w-7xl mx-auto px-4">
+  <section class="py-20 bg-gray-50 dark:bg-gray-800">
+    <div class="container mx-auto px-6">
       <slot />
     </div>
   </section>
